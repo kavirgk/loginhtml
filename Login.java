321fcba12999e449b8f0bfae20f139b8df754584
kavirgk <77719066+kavirgk@users.noreply.github.com>
2021-01-21 21:51:46 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,32 @@
+package servlogin;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Login extends HttpServlet {
+ 
+ 
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        
+        if(Validate.checkUser(email, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
+    }  
+}
---
+++ Welcome.java
@@ -0,0 +1,18 @@
+package servlogin;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Welcome extends HttpServlet {
+
+  
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Welcome user");
+      }  
+}
